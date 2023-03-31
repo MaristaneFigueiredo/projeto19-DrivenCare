c71fc42278e4e8856e37cd539501b378f2f09375
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-03-31 17:25:19 -0300

    refactor: schema user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import usersRouter from "./usersRouter.js";
+// import appointmentsRouter from "./appointmentsRouter.js"
+// import schedulesRouter from "./schedulesRouter.js"
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
+// routes.use("/appointments", appointmentsRouter)
+// routes.use("/schedules", schedulesRouter)
 
 export default routes
 
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -12,14 +12,14 @@ import joi from "joi"
 // lastname: Joi.alternatives().conditional('type', { is: 1, then: Joi.string().required() }),
 
 export const userSchema = joi.object({
-        name: joi.string().min(2).required(),
-        email:joi.string().email().required(),
-        password:joi.string().required(), 
-        typeUser:joi.string().allow("D", "P").required(),   
-        //specialtyId:joi.alternatives.conditional('typeUser', { is: "D", then: joi.number().required() })
-        //locationId
-        specialtyId:joi.alternatives().conditional(
-                'typeUser', { is: "D", then: joi.number().required() }
-        )
+    name: joi.string().max(100).required(),
+    email: joi.string().email().max(100).required(),
+    password: joi.string().max(100).required(),
+    userType: joi.valid("D", "P"),
+    locationId: joi.alternatives().conditional('userType', {
+        is: "D",
+        then: joi.number().required(),
+        otherwise: joi.optional()
+    })
 
 })
