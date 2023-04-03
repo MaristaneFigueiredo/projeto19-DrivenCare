c40b20fb540f691df5692f0ab608724ab833946e
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-04-03 17:12:31 -0300

    reafactor: doctors change status appontment

--- src/controllers/appointmentsController.js
+++ src/controllers/appointmentsController.js
@@ -58,8 +58,8 @@ async function updateAppointmentStatus(req, res,next) {
 }
 
 export default {
-    createAppoitment,
+    createAppoitment,    
+    listPatientAppointments,
+    listDoctorAppointments,
     updateAppointmentStatus,
-     listPatientAppointments,
-     listDoctorAppointments
 }
